d39a0530328ace74a53c947a6dfb71278c401dae
Animesh Murmu <am2646374@gmail.com>
2023-11-16 11:05:18 +0530

    Fix submitPost error handler

--- lib/application/post/post_cubit.dart
+++ lib/application/post/post_cubit.dart
@@ -29,51 +29,55 @@ class PostCubit extends Cubit<PostState> {
   /// Returns [PostLoading], [PostSubmitSuccessful], [PostSubmitFailed]
   void submitPost(Post post) async {
     emit(PostLoading());
-    final posts = firestore.collection('posts');
-    final uid = auth.currentUser!.uid;
-    final doc = posts.doc();
-    final ref = storage.ref(uid).child(doc.id);
-
-    final extension = (post.video.mimeType ?? 'video/webm').split('/').last;
-
-    final videoRef = ref.child('video.$extension');
-    final thumbnailref = ref.child('thumbnail.jpeg');
-
-    final videoMeta = SettableMetadata(contentType: post.video.mimeType);
-    final thumbnailMeta = SettableMetadata(contentType: post.video.mimeType);
-
-    if (kIsWeb) {
-      videoRef
-          .putData(await post.video.readAsBytes(), videoMeta)
-          .catchError(() => emit(PostSubmitFailed()));
-      thumbnailref
-          .putData(await post.thumbnail.readAsBytes(), thumbnailMeta)
-          .catchError(() => emit(PostSubmitFailed()));
-    } else {
-      await videoRef
-          .putFile(File(post.video.path), videoMeta)
-          .catchError(() => emit(PostSubmitFailed()));
-      await thumbnailref
-          .putFile(File(post.thumbnail.path), thumbnailMeta)
-          .catchError(() => emit(PostSubmitFailed()));
-    }
-
-    final data = FPost(
-        username: account.currentUser!.username,
-        location: GeoPoint(post.location.latitude, post.location.longitude),
-        hLocation: post.hLocation!,
-        video: await videoRef.getDownloadURL(),
-        thumbnail: await thumbnailref.getDownloadURL(),
-        title: post.title!,
-        category: post.category!,
-        uid: uid,
-        createdAt: Timestamp.now());
-
     try {
-      await doc.set(data.toMap());
-      emit(PostSubmitSuccessful(post: data, id: doc.id));
-    } on Exception catch (e) {
-      debugPrint(e.toString());
+      final posts = firestore.collection('posts');
+      final uid = auth.currentUser!.uid;
+      final doc = posts.doc();
+      final ref = storage.ref(uid).child(doc.id);
+
+      final extension = (post.video.mimeType ?? 'video/webm').split('/').last;
+
+      final videoRef = ref.child('video.$extension');
+      final thumbnailref = ref.child('thumbnail.jpeg');
+
+      final videoMeta = SettableMetadata(contentType: post.video.mimeType);
+      final thumbnailMeta = SettableMetadata(contentType: post.video.mimeType);
+
+      if (kIsWeb) {
+        await videoRef
+            .putData(await post.video.readAsBytes(), videoMeta)
+            .catchError(() => emit(PostSubmitFailed()));
+        await thumbnailref
+            .putData(await post.thumbnail.readAsBytes(), thumbnailMeta)
+            .catchError(() => emit(PostSubmitFailed()));
+      } else {
+        await videoRef
+            .putFile(File(post.video.path), videoMeta)
+            .catchError(() => emit(PostSubmitFailed()));
+        await thumbnailref
+            .putFile(File(post.thumbnail.path), thumbnailMeta)
+            .catchError(() => emit(PostSubmitFailed()));
+      }
+
+      final data = FPost(
+          username: account.currentUser!.username,
+          location: GeoPoint(post.location.latitude, post.location.longitude),
+          hLocation: post.hLocation!,
+          video: await videoRef.getDownloadURL(),
+          thumbnail: await thumbnailref.getDownloadURL(),
+          title: post.title!,
+          category: post.category!,
+          uid: uid,
+          createdAt: Timestamp.now());
+
+      try {
+        await doc.set(data.toMap());
+        emit(PostSubmitSuccessful(post: data, id: doc.id));
+      } on Exception catch (e) {
+        debugPrint(e.toString());
+        emit(PostSubmitFailed());
+      }
+    } catch (_) {
       emit(PostSubmitFailed());
     }
   }
